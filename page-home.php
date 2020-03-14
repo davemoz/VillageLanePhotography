18294aa9c619cfa027eb2b0f6383b22b4b423ef6
davemoz <davejmoz@gmail.com>
2020-03-14 18:07:46 -0400

    Tweak homepage

--- page-home.php
+++ page-home.php
@@ -13,7 +13,6 @@ get_header(); ?>
 	global $post;
 	$featured_img_url = get_the_post_thumbnail_url($post->ID, 'large');
 ?>
-<!--<div id="home-banner" style="background-image: url(<?php echo $featured_img_url; ?>)"></div>-->
 
 <?php the_content(); ?>
 
