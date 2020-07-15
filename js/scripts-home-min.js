012f69f83b5805002ab4ea4060dda1203488eef1
davemoz <davejmoz@gmail.com>
2020-07-15 10:48:52 -0400

    Test no flickity

--- js/scripts-home-min.js
+++ js/scripts-home-min.js
@@ -1,2 +1,2 @@
-!function(){var e=document.querySelector(".blocks-gallery-grid");e.classList.remove("is-hidden"),e.offsetHeight;var a=new Flickity(e,{autoPlay:5e3,draggable:!0,pageDots:!1,wrapAround:!0,prevNextButtons:!0,cellAlign:"center",imagesLoaded:!0,lazyLoad:!0});e.addEventListener("mouseleave",(function(e){a.playPlayer()}))}();
+
 //# sourceMappingURL=scripts-home-min.js.map
